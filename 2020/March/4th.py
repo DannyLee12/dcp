2021f08d03428a5fb032133fecb3667c4b795ad2
Daniel Lee <daniel@dotmodus.com>
2020-03-04 13:31:24 +0000

    :sparkles: Makes sense - had to ask for help though

---
+++ 2020/March/4th.py
@@ -0,0 +1,20 @@
+"""
+This problem was asked by Facebook.
+Given a stream of elements too large to store in memory, pick a random element
+from the stream with uniform probability.
+"""
+import random
+
+
+def random_stream(stream):
+    """Recalculate the random value for each value in the stream"""
+    for i, x in enumerate(stream):
+        if i == 0:
+            random_value = x
+        elif random.randint(1, 1 + i) == 1:
+            random_value = x
+    return random_value
+
+
+if __name__ == '__main__':
+    print(random_stream([1,2,3,4,5]))
