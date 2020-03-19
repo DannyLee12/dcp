0f5fd6316e281aa6d87bf259bbf5133a91b9ee64
Daniel Lee <daniel@dotmodus.com>
2020-03-19 15:20:30 +0000

    :sparkles: Quite a neat solution I must say

---
+++ 2020/March/27.py
@@ -0,0 +1,30 @@
+"""
+Given a string of round, curly, and square open and closing brackets, return
+whether the brackets are balanced (well-formed).
+
+For example, given the string "([])[]({})", you should return true.
+
+Given the string "([)]" or "((()", you should return false.
+"""
+
+
+def well_formed(s: str) -> bool:
+    """Use a stack"""
+    stack = []
+    for c in s:
+        if c in ["(", "{", "["]:
+            stack.append(c)
+        else:
+            if c == '}' and stack[-1] != '{' or \
+               c == ')' and stack[-1] != '(' or \
+               c == ']' and stack[-1] != '[':
+                return False
+            stack.pop()
+
+    return len(stack) == 0
+
+
+if __name__ == '__main__':
+    assert well_formed("([])[]({})") is True
+    assert well_formed("([)]") is False
+    assert well_formed("((()") is False
