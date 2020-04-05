450299eeef417d4c2b160bf3e46a4921f401beb5
Daniel Lee <daniel@dotmodus.com>
2020-04-05 13:20:21 +0100

    :sparkles: Improved the worst case time complexity to O(len(s+p)) where s is the string and p is the pallindrome

--- 2020/04_April/46.py
+++ 2020/04_April/46.py
@@ -14,6 +14,7 @@ def is_palindrome(s: str) -> bool:
 
 def pal(s: str, current: str=None) -> str:
     """
+    SUPERCEEDED by pal2
     Return the longest palindrome in a string
     """
     if not current:
@@ -33,6 +34,38 @@ def pal(s: str, current: str=None) -> str:
     return pal(s[1:], current)
 
 
+def pal2(s: str) -> str:
+    """Return the longest substring in O(len(s))"""
+    # Consider Odd case
+    longest = s[0]
+    n = len(s)
+    for i, letter in enumerate(s):
+        x = 1
+        y = 0
+        try:
+            while s[i - x] == s[i + x]:
+                if i - x < 0 or i + x >= n:
+                    break
+                candidate = s[i - x:i + x + 1]
+                if len(candidate) > len(longest):
+                    longest = candidate
+                x += 1
+            while s[i - y] == s[i + y - 1]:
+                if i - y < 0 or i + y - 1 >= n:
+                    break
+                candidate2 = s[i - y:i + y]
+                if len(candidate2) > len(longest):
+                    longest = candidate2
+                y += 1
+        except IndexError:
+            pass
+
+
+    return longest
+
 if __name__ == '__main__':
-    print(pal("banana"))
-    print(pal("aabcdcb"))
+    # print(pal("banana"))
+    # print(pal("aabcdcb"))
+    print(pal2("banana"))
+    print(pal2("aabcdcb"))
+    print(pal2("banna"))
