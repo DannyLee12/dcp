e13736b1dd681b3c74fa79d180f270975884d74e
Daniel Lee <daniel@dotmodus.com>
2020-04-27 09:21:01 +0100

    :sparkles: Runs in n Log(n)

---
+++ 2020/04_April/69.py
@@ -0,0 +1,21 @@
+"""
+Given a list of integers, return the largest product that can be made by
+multiplying any three integers.
+
+For example, if the list is [-10, -10, 5, 2], we should return 500, since
+that's -10 * -10 * 5.
+
+You can assume the list has at least three integers.
+"""
+
+
+def largest_product(l: list) -> int:
+    """Return the largest product of three integers"""
+    n = len(l)
+    l.sort()
+    # Few options:
+    return max((l[0] * l[1] * l[-1]), (l[-3] * l[-2] * l[-1]))
+
+
+if __name__ == '__main__':
+    print(largest_product([-10, -10, 5, 2]))
