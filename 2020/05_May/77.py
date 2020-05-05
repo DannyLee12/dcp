b633d55339181f42cf9194cc8d5ac580a5d19df6
Daniel Lee <daniel@dotmodus.com>
2020-05-05 12:37:33 +0100

    :sparkles: N2 time complexity. Good enough?

---
+++ 2020/05_May/77.py
@@ -0,0 +1,25 @@
+"""
+Given a list of possibly overlapping intervals, return a new list of intervals
+where all overlapping intervals have been merged.
+
+The input list is not necessarily ordered in any way.
+
+For example, given [(1, 3), (5, 8), (4, 10), (20, 25)], you should return
+[(1, 3), (4, 10), (20, 25)].
+"""
+
+
+def new_intervals(l: list) -> list:
+    """return a new interval without overlapping intervals"""
+    for i, s in enumerate(l):
+        for t in l:
+            if s == t:
+                continue
+            if s[0] > t[0] and s[1] < t[1]:
+                l.pop(i)
+
+    return l
+
+
+if __name__ == '__main__':
+    print(new_intervals([(1, 3), (5, 8), (4, 10), (20, 25)]))
