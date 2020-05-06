e65d8aaec29386090064a0c86595f591bbffc418
Daniel Lee <daniel@dotmodus.com>
2020-05-06 09:41:26 +0100

    :sparkles: NlogN is faster than N2

--- 2020/05_May/77.py
+++ 2020/05_May/77.py
@@ -21,5 +21,23 @@ def new_intervals(l: list) -> list:
     return l
 
 
+def new_ints(l: list) -> list:
+    """return a new interval without overlapping intervals"""
+    result = []
+    for i, (j, k) in enumerate(sorted(l, key=lambda x: x[0])):
+        if not result:
+            result.append((j, k))
+        else:
+            if prev[0] < j and prev[1] < k:
+                result.append((j, k))
+        prev = j, k
+
+    return result
+
+
+
+
+
 if __name__ == '__main__':
-    print(new_intervals([(1, 3), (5, 8), (4, 10), (20, 25)]))
+    # print(new_intervals([(1, 3), (5, 8), (4, 10), (20, 25)]))
+    print(new_ints([(1, 3), (5, 8), (4, 10), (20, 25)]))
