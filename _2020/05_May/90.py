758049576ad68bb5f288615e39116f0a3cd2bc7d
Daniel Lee <daniel@dotmodus.com>
2020-05-18 09:10:28 +0100

    :zap: Improved performance
    This approach uses sets to determine which values can be chosen from
    and the random.choice() function to choose a random value in an iterable
    
    i.e. {1, 3, 5} - {1, 5} = {3}
    
    Create a list across the range from 0 to n - 1 and remove the elements
    in l. Then choose a random value in that new list

--- _2020/05_May/90.py
+++ _2020/05_May/90.py
@@ -2,7 +2,7 @@
 Given an integer n and a list of integers l, write a function that randomly
 generates a number from 0 to n-1 that isn't in l (uniform).
 """
-from random import randint
+from random import randint, choice
 
 
 def gen_random(n: int, l: list) -> int:
@@ -15,5 +15,14 @@ def gen_random(n: int, l: list) -> int:
     return i
 
 
+def gen_random_improved(n: int, l: list) -> int:
+    """Randomly generate a number from 0 to n-1 that is not in l"""
+    r = set([x for x in range(n-1)]) - set(l)
+
+    return choice(list(r))
+
+
 if __name__ == '__main__':
     assert gen_random(5, [2, 3]) in [0, 1, 4]
+    for _ in range(100):
+        assert gen_random_improved(5, [2, 3]) in [0, 1, 4]
