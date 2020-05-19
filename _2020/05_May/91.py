134ca8f7f43ce122c7711c94c74ac47daf2c6853
Daniel Lee <daniel@dotmodus.com>
2020-05-19 16:36:11 +0100

    :books: Should have tried this earlier to be honest

--- _2020/05_May/91.py
+++ _2020/05_May/91.py
@@ -11,16 +11,6 @@ for f in functions:
 
 """
 
-global n
-n = 0
-
-
-def fn():
-    global n
-    n += 1
-    return n - 1
-
-
 if __name__ == '__main__':
     functions = []
     for i in range(10):
@@ -34,18 +24,10 @@ if __name__ == '__main__':
     #  the loop will print '9' ten times
 
     # Expected Behavior:
-    # 1. - Using another function with a global variable
+
     functions = []
     for i in range(10):
-        functions.append(fn)
+        functions.append(lambda i=i: i)
 
-    for _, f in enumerate(functions):
+    for f in functions:
         print(f())
-
-    # 2. Passing in a variable
-    functions = []
-    for i in range(10):
-        functions.append(lambda i: i)
-
-    for i, f in enumerate(functions):
-        print(f(i))
