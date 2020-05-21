087c0b75e2f6e2b522b3180727696f46ea8ccb36
Daniel Lee <daniel@dotmodus.com>
2020-05-21 12:38:20 +0100

    :truck: WIP attempt at another :roll_eyes: Tree problem - moved some Tree code to the tools folder

---
+++ _2020/05_May/93.py
@@ -0,0 +1,31 @@
+"""
+Given a tree, find the largest tree/subtree that is a BST.
+
+Given a tree, return the size of the largest tree/subtree that is a BST.
+"""
+
+from _2020 import Tree, valid_tree
+
+
+def larget_bst(t: Tree) -> Tree:
+    """Return the largest bst in a Tree"""
+
+    def size_tree(t, size=0):
+        """Calculate the size of a tree"""
+        if isinstance(t.left, Tree) and isinstance(t.right, Tree):
+            return size_tree(t.left, size + 1) + size_tree(t.right, size + 1)
+        else:
+            return size + 1
+
+    tv = valid_tree(t)
+    if tv:
+        return tv
+
+
+
+if __name__ == '__main__':
+    vt = Tree(2, 1, 3, root=True)
+    vt2 = Tree(2, Tree(2, 1), Tree(4, right=5), root=True)
+
+    # print(larget_bst(vt))
+    print(larget_bst(vt2))
--- _2020/tools.py
+++ _2020/tools.py
@@ -36,6 +36,43 @@ class Tree:
         return f"{tree.left} <- {tree.node} -> {tree.right}"
 
 
+def valid_tree(t: Tree, flag: str=None, calls=0) -> (bool, int):
+    """
+    Return whether a binary search tree is valid
+    """
+    calls += 1
+    if isinstance(t.left, Tree):
+        t.left = valid_tree(t.left, 'l', calls)
+    if isinstance(t.right, Tree):
+        t.right = valid_tree(t.right, 'r', calls)
+    if t.left and t.right:
+        if t.left <= t.node <= t.right:
+            if t.root:
+                return True, calls
+            elif flag == 'l':
+                # On the left side of the Binary Tree, we are concerned with
+                # the larger value (t.right) assuming that tree is valid
+                return t.right
+            elif flag == 'r':
+                return t.left
+            else:
+                return t.node
+    elif t.left:
+        if t.left <= t.node:
+            if t.root:
+                return True, calls
+            else:
+                return t.node
+    elif t.right:
+        if t.node <= t.right:
+            if t.root:
+                return True, calls
+            else:
+                return t.node
+
+    return t.left <= t.node <= t.right, calls
+
+
 class LinkedList:
     def __init__(self, data, next=None):
         self.data = data
