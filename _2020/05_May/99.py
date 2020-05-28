7e891920c387b57c48b07a15b2f1a36bcd24f87e
Daniel Lee <daniel@dotmodus.com>
2020-05-28 08:53:15 +0100

    :books: Managed to find a truly O(N) solution
    As expected the space complexity of this solution leaves much
    to be desired. That being said, it's a neat enough solution
    for what it needs to do.

--- _2020/05_May/99.py
+++ _2020/05_May/99.py
@@ -23,6 +23,25 @@ def in_set(i: int, s: set, total: int = 0, visited=None) -> int:
     return 0
 
 
+def longest_consecutive(nums):
+    max_len = 0
+    bounds = dict()
+    for num in nums:
+        if num in bounds:
+            continue
+        left_bound, right_bound = num, num
+        if num - 1 in bounds:
+            left_bound = bounds[num - 1][0]
+        if num + 1 in bounds:
+            right_bound = bounds[num + 1][1]
+        bounds[num] = left_bound, right_bound
+        bounds[left_bound] = left_bound, right_bound
+        bounds[right_bound] = left_bound, right_bound
+        max_len = max(right_bound - left_bound + 1, max_len)
+
+    return max_len
+
+
 if __name__ == '__main__':
     l = [100, 4, 200, 1, 3, 2]
     s = set()
@@ -32,3 +51,4 @@ if __name__ == '__main__':
         max_so_far = max(max_so_far, in_set(i, s))
 
     print(max_so_far)
+    print(longest_consecutive(l))
