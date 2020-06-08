318ca99b0f4fb0c772dcb9929491eeca1a9bf58e
Daniel Lee <daniel@dotmodus.com>
2020-06-08 09:21:12 +0100

    :zap: Improve performance
    New algorithm is 300 times quicker over 1000 iterations.
    
    This appoach runs the matching in O(N), with a single sort
    per i in range, this could potentially be slow, and could
    still be improved be keeping the previously sorted string,
    this seems like an unneccessary optimization and we will
    pay for it in readability.
    Some results:
      Initial algo: Time taken = 3.3350119590759277 seconds
      Faster algo: Time takes = 0.017987728118896484 Seconds

--- _2020/06_June/111.py
+++ _2020/06_June/111.py
@@ -6,6 +6,7 @@ For example, given that W is "ab", and S is "abxaba", return 0, 3, and 4.
 """
 
 from itertools import permutations
+import time
 
 
 def find_anagrams(w: str, s: str) -> tuple:
@@ -28,6 +29,29 @@ def find_anagrams(w: str, s: str) -> tuple:
     return positions
 
 
+def find_anagrams_fast(w: str, s: str) -> tuple:
+    """Return positions of all anagrams of w in s, but fast"""
+    positions = tuple()
+    n = len(w)
+    sw = sorted(w)  # Lexicographically sort w
+    for i in range(len(s) - n + 1):
+        if sorted(s[i:i+n]) == sw:
+            positions += (i, )
+
+    return positions
+
+
 if __name__ == '__main__':
     assert find_anagrams("ab", "abxaba") == (0, 3, 4)
     assert find_anagrams("ab", "acaadf") == tuple()
+    t1 = time.time()
+    for _ in range(1000):
+        find_anagrams("abcdefg", "abcdefgabcdefgabcdefgabcdefgabcdefg")
+    print(f"Time taken = {time.time() - t1} seconds")
+
+    assert find_anagrams_fast("ab", "abxaba") == (0, 3, 4)
+    assert find_anagrams_fast("ab", "acaadf") == tuple()
+    t2 = time.time()
+    for _ in range(1000):
+        find_anagrams_fast("abcdefg", "abcdefgabcdefgabcdefgabcdefgabcdefg")
+    print(f"Time takes = {time.time() - t2} Seconds")
