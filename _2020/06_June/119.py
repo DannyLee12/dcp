4d421be7db038bba25eb23736eded2f81c768434
Daniel Lee <daniel@dotmodus.com>
2020-06-16 16:56:20 +0100

    :books: Saw this implementation, reworked it with comments
    Guess the logic is relatively clear once you get it.
    Loop through sorted intervals until an interval that
    cannot fit in your range is found. Then do that again
    with the next few intervals

--- _2020/06_June/119.py
+++ _2020/06_June/119.py
@@ -7,35 +7,37 @@ numbers that covers all these intervals is {3, 6}.
 """
 
 
-def smallest_set(l: list) -> set:
-    """Return the smallest set that covers all the intervals"""
-    s1 = set()
-    confirmed_sets = set()
-    unconfirmed = []
-    for interval in l:
-        un_flag = False
-        for val in interval:
-            if val in s1:
-                confirmed_sets.add(val)
-                un_flag = True
+def covering(l: list) -> set:
+    """Determine the smallest set that covers all intervals"""
+    result = set()
+    i = 0
+    n = len(l)
+    l.sort(key=lambda x: x[0])
+
+    while i < n:
+        # Iterate through the list of intervals that are NOT in the current
+        # set
+        interval = l[i]
+
+        # Loop through the remaining intervals until one doesn't overlap
+        while i < n:
+            if intersecting(l[i], interval):
+                # Recalculate the interval such that it just overlaps each
+                interval = (max(l[i][0], interval[0]), min(l[i][1], interval[1]))
+                i += 1
             else:
-                if not un_flag:
-                    un_flag = True
-                    unconfirmed.append(interval)
-            s1.add(val)
-
-    for int in unconfirmed:
-        valid = False
-        for val in int:
-            if val in confirmed_sets:
-                valid = True
+                print("Value not intersecting")
                 break
-        if not valid:
-            confirmed_sets.add(int[0])
 
-    return confirmed_sets
+        result.add(interval[0])
+    return result
+
+
+def intersecting(x, y):
+    """Determine if x and y intersect"""
+    return not (x[0] > y[1] or y[0] > x[1])
 
 
 if __name__ == '__main__':
-    print(smallest_set([[0, 3], [2, 6], [3, 4], [6, 9]]))
-    print(smallest_set([[0, 3], [1, 12], [2, 6], [3, 4], [6, 12], [13, 17]]))
+    print(covering([[0, 3], [2, 6], [3, 4], [6, 9]]))
+    print(covering([[0, 3], [1, 12], [2, 6], [3, 4], [6, 12], [13, 17]]))
