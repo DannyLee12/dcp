4845b0bd554a4e9be8b683aea049946c12f8f38b
Daniel Lee <daniel@dotmodus.com>
2020-06-18 09:30:23 +0100

    :zap: Improve performance
    Instead of using the incredibly slow recursive solution, we
    can use a slightly faster recursive solution.
    
    Basically, one way to determine a palindrome is to head from the
    end and the front at the same time, and check that the chars all
    match. In the case that one of the chars does not match, we
    recursively call the function again with the string without the
    leading or trailing character. Since we are only allowed k
    changes, this is a backtracking algo that returns true if a
    solution is found.
    
    We return True once there is only one char remaining, or we return
    True once the head index and the tail index are the same.

--- _2020/06_June/121.py
+++ _2020/06_June/121.py
@@ -25,7 +25,30 @@ def make_pal(s: str, k: int) -> bool:
     return False
 
 
+def do_better(s: str, k: int) -> bool:
+    """Head from the back and the front and see if we can make a palindrome"""
+    if k < 0:
+        return False
+    head_ptr = 0
+    tail_ptr = len(s) - 1
+    if len(s) <= 1:
+        return True
+    while s[head_ptr] == s[tail_ptr]:
+        if tail_ptr <= head_ptr:
+            return True
+        head_ptr += 1
+        tail_ptr -= 1
+    # If they do not match, try again by popping off the front or back char
+    if do_better(s[head_ptr:tail_ptr], k-1):
+        return True
+    elif do_better(s[head_ptr+1:tail_ptr+1], k-1):
+        return True
+
+
 if __name__ == '__main__':
     assert make_pal("waterrfetawx", 2)
     assert not make_pal("waterrfetawx", 1)
     assert make_pal("waterrfetawx", 3)
+    assert do_better("waterrfetawx", 2)
+    assert not do_better("waterrfetawx", 1)
+    assert do_better("waterrfetawx", 3)
