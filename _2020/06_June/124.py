4e4c9972b26a80efe5c666553442e3715d8ce819
Daniel Lee <daniel@dotmodus.com>
2020-06-22 08:55:15 +0100

    :sparkles: Neat recursive solution
    There should be a better way to do this, using probabilities and
    math instead of resursion. A loop will also end up at the same
    place, but perhaps significantly quicker. Anyway, I find this
    easy enough to read.

---
+++ _2020/06_June/124.py
@@ -0,0 +1,27 @@
+"""
+You have n fair coins and you flip them all at the same time. Any that come up
+tails you set aside. The ones that come up heads you flip again. How many
+rounds do you expect to play before only one coin remains?
+
+Write a function that, given n, returns the number of rounds you'd expect to
+play until one coin remains.
+"""
+from random import random
+
+
+def flip_coins(n: int, count: int = 0) -> int:
+    """How many times do you need to flip coins until only one remains"""
+    heads = 0
+    for _ in range(n):
+        if random() > 0.5:
+            heads += 1
+    if heads <= 1:
+        return count
+
+    return flip_coins(heads, count + 1)
+
+
+if __name__ == '__main__':
+    print(flip_coins(100))
+    print(flip_coins(10))
+    print(flip_coins(1))
