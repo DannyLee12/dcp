f0e59cd3778982884e8396d06f13f2e3cd8e676d
Daniel Lee <daniel@dotmodus.com>
2020-06-22 08:58:52 +0100

    :books: Use a mathematical formula rather
    Fair coins, you lose half of the coins each flip.
    In maths that's log2(n).

--- _2020/06_June/124.py
+++ _2020/06_June/124.py
@@ -7,6 +7,7 @@ Write a function that, given n, returns the number of rounds you'd expect to
 play until one coin remains.
 """
 from random import random
+from math import log
 
 
 def flip_coins(n: int, count: int = 0) -> int:
@@ -21,7 +22,14 @@ def flip_coins(n: int, count: int = 0) -> int:
     return flip_coins(heads, count + 1)
 
 
+def coins(n):
+    return log(n, 2)
+
+
 if __name__ == '__main__':
     print(flip_coins(100))
     print(flip_coins(10))
     print(flip_coins(1))
+    print(coins(100))
+    print(coins(10))
+    print(coins(1))
