8e2eb277a488efc7ed5d3bd77bcef4ae75611768
Daniel Lee <daniel@dotmodus.com>
2020-06-26 12:22:26 +0100

    :zap: Performance improvements
    Use a binary search approach but also get the upper and lower
    bounds by doubling the bounds each time. This makes it quck
    for very large numbers. Then a simple binary search to return
    the value. Since floating points are ugly to work with, the
    epsilon here is chosed to 0.001 and then the midpoint is returned.
    This is a good approximation.

--- _2020/06_June/129.py
+++ _2020/06_June/129.py
@@ -16,8 +16,29 @@ def sqrt_(n: float) -> float:
         counter /= 10
 
 
+def sqrt_bs(n: float) -> float:
+    """Try a binary search approach"""
+    if n < 1:
+        return sqrt_(n)
+    lower, upper = 1, float("inf")
+    while lower * lower < n:
+        lower *= 2
+    lower /= 2
+    upper = lower * 2
+    while abs(lower - upper) > 0.001:
+        midpoint = (upper + lower) / 2
+        if midpoint * midpoint > n:
+            upper = midpoint
+        elif midpoint * midpoint < n:
+            lower = midpoint
+        else:
+            return midpoint
+    return midpoint
+
+
 if __name__ == '__main__':
-    assert sqrt_(9) == 3
-    assert sqrt_(81) == 9
-    assert abs(sqrt_(2.25) - 1.5) < 0.0001
-    assert abs(sqrt_(0.36) - 0.6) < 0.001
+    assert sqrt_bs(9) == 3
+    assert sqrt_bs(81) == 9
+    assert abs(sqrt_bs(2.25) - 1.5) < 0.0001
+    assert abs(sqrt_bs(0.36) - 0.6) < 0.0001
+    print(sqrt_bs(8))
