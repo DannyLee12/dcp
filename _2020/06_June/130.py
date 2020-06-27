98782c483645e6d3009434773ec9e6031920dbb9
Daniel Lee <daniel@dotmodus.com>
2020-06-27 10:40:54 +0100

    :bug: Fixing a non working program
    This solution's time complexity is pretty dire. I guess that we
    could attempt to use Kadane's algorithm in one way or another
    so that each loop runs in O(N)

--- _2020/06_June/130.py
+++ _2020/06_June/130.py
@@ -18,9 +18,36 @@ def greedy(l: list, k: int) -> int:
         else:
             profit += x - hold
             k -= 1
+            if not k:
+                break
 
     return profit
 
 
+def largest_profits(l: list, k:  int) -> int:
+    """Always return the greatest profit, run k times"""
+    total_profit = 0
+    while k:
+        n = len(l)
+        largest_profit = 0
+        large_i = -1
+        large_j = -1
+        for i in range(n):
+            for j in range(i, n):
+                if l[j] - l[i] > largest_profit:
+                    large_j = j
+                    large_i = i
+                    largest_profit = l[j] - l[i]
+        k -= 1
+        l.pop(large_i)
+        l.pop(large_j - 1)  # Lose one position due to above pop
+        total_profit += largest_profit
+
+    return total_profit
+
+
 if __name__ == '__main__':
     assert greedy([5, 2, 4, 0, 1], 2) == 3
+    # assert largest_profits([5, 2, 4, 0, 1], 2) == 3
+    print(greedy([5, 0, 1, 2, 4], 1))  # 1, should be 4 Buy 0 sell 4
+    assert largest_profits([5, 0, 1, 2, 4], 1) == 4
