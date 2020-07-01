69e35fa695e762277eb7ae2340bd08db2bf5ce11
Daniel Lee <daniel@dotmodus.com>
2020-07-01 09:51:55 +0100

    :sparkles: Implement a SparseArray
    A dictionary seems like a good approach for this question.
    Easy to understand, relatively fast and simple.

---
+++ _2020/06_June/134.py
@@ -0,0 +1,43 @@
+"""
+You have a large array with most of the elements as zero.
+
+Use a more space-efficient data structure, SparseArray, that implements the
+same interface:
+
+    init(arr, size): initialize with the original large array and size.
+    set(i, val): updates index at i with val.
+    get(i): gets the value at index i.
+
+"""
+from random import random
+
+
+class SparseArray:
+    d = {}
+
+    def init(self, arr: list, size: int):
+        for i, x in enumerate(arr):
+            if x:  # Ignore 0s
+                self.d[i] = x
+
+    def set(self, i, val):
+        self.d[i] = val
+
+    def get(self, i):
+        return self.d[i]
+
+
+if __name__ == '__main__':
+    sa = SparseArray()
+    large_list = []
+    for x in range(1000):
+        if random() < 0.1:
+            large_list.append(int(random() * 100))
+        large_list.append(0)
+
+    sa.init(large_list, len(large_list))
+
+    sa.set(20, -1)
+    sa.set(12000, -1)
+    assert sa.get(20) == -1
+    assert sa.get(12000) == -1
