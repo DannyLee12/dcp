134644ce6b4872b097f2c55a666d20ee11d8d9ad
Daniel Lee <daniel@dotmodus.com>
2020-07-15 09:19:22 +0100

    :sparkles: Implement the actual sorting algo
    Loop through the list and reverse any sequences that
    are out of order. Runs in 0(N2).

--- _2020/07_July/147.py
+++ _2020/07_July/147.py
@@ -12,5 +12,22 @@ def reverse(lst, i, j):
     return lst[:i] + rev(lst[i:j+1]) + lst[j+1:]
 
 
+def pancake_sort(l: list) -> list:
+    """Sort a list using reverse"""
+    i = 0
+    j = len(l)
+    while i < j:
+        for idx, x in enumerate(l):
+            if idx <= i:
+                continue
+            if x < l[i]:
+                l = reverse(l, i, idx)
+        i += 1
+    return reverse(l, i, j)
+
+
 if __name__ == '__main__':
-    assert reverse([1,2,3,4,5], 1, 3) == [1, 4, 3, 2, 5]
+    assert pancake_sort([4, 5, 3, 2, 1]) == [1, 2, 3, 4, 5]
+    assert pancake_sort([1, 3, 2, 4, 5]) == [1, 2, 3, 4, 5]
+    assert pancake_sort([5, 1, 4, 2, 3]) == [1, 2, 3, 4, 5]
+
