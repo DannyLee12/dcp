3bd92972e0615ee8358f1711c6537fe4914e6486
Daniel Lee <daniel@dotmodus.com>
2020-07-15 11:19:33 +0100

    :sparkles: Recursively use the base 2 gray codes
    The first set of grey codes we can prepend a 0 to.
    The second set of grey codes we prepend a 1 to. Since we
    can only change one bit per iteration, we reverse the
    second set of numbers so that the only number that is
    different is the prepended 0 -> 1. Then since the
    codes only change once per cycle on the way up, they
    will also only change once per cycle in the reverse
    direction.

---
+++ _2020/07_July/148.py
@@ -0,0 +1,23 @@
+"""
+Gray code is a binary code where each successive value differ in only one bit,
+as well as when wrapping around. Gray code is common in hardware so that we
+don't see temporary spurious values during transitions.
+
+Given a number of bits n, generate a possible gray code for it.
+
+For example, for n = 2, one gray code would be [00, 01, 11, 10].
+"""
+
+
+def gray(n: int) -> list:
+    """Return gray codes"""
+    if n == 2:
+        return [[0, 0], [1, 0], [1, 1], [0, 1]]
+    else:
+        return [[0] + x for x in gray(n-1)] + [[1] + x for x in reversed(gray(n-1))]
+
+
+if __name__ == '__main__':
+    print(gray(2))
+    print(gray(3))
+    print(gray(4))
