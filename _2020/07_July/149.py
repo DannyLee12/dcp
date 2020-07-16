c7f8d6bf0de039389e9ae657e1056ef919485fce
Daniel Lee <daniel@dotmodus.com>
2020-07-16 09:20:31 +0100

    :zap: Improve performance
    This is almost 1000 times quicker over a 500k entry sum.
    
    This approach works much better for similar lookups. In the
    instance that we already have a similar lookup, this approach
    will rather use that value and add the rest of the numbers to
    that number. This is only used when it's quicker to do the
    lookup (x - j) than to simply add all the numbers (j - i).
    
    This approach does have some limitations, for one when we have lots
    of keys in each dictionary it may take long to find a key that is
    faster than a clean lookup, but in "general" it should be more
    performant.

--- _2020/07_July/149.py
+++ _2020/07_July/149.py
@@ -8,6 +8,7 @@ which is 5.
 You can assume that you can do some pre-processing. sum() should be optimized
 over the pre-processing step.
 """
+from time import time_ns
 
 
 class L:
@@ -16,12 +17,37 @@ class L:
         self.l = l
 
     def sum(self, i, j):
-        key = f"{i}{j}"
-        if key not in self.cache:
-            self.cache[key] = sum(self.l[i: j])
-        return self.cache[key]
+        if i not in self.cache:
+            self.cache[i] = {j: sum(self.l[i:j])}
+        else:
+            if j in self.cache[i]:
+                # Direct match
+                pass
+            else:
+                for x in self.cache[i].keys():
+                    # Calculate which is quicker, sum or lookup + smaller sum
+                    if j - x < j - i:
+                        value = self.cache[i][x] + sum(self.l[x:j])
+                self.cache[i][j] = value
+        return self.cache[i][j]
 
 
 if __name__ == '__main__':
     l = L([1, 2, 3, 4, 5])
     assert l.sum(1, 3) == 5
+    l_large = [x for x in range(1000000)]
+    l2 = L(l_large)
+    # Create cache
+    t = time_ns()
+    print(l2.sum(1, 500000))
+    t1 = time_ns() - t
+    print(f"Time taken for 5000 = {t1}")
+    # Get a slightly bigger object
+    t = time_ns()
+    print(l2.sum(1, 500001))
+    t2 = time_ns() - t
+    print(f"Time taken for 5001 = {t2}")
+    # Confirm the speedup
+    assert t2 * 100 < t1
+    # Confirm the logic is sound
+    assert l.sum(1, 4) == 9
