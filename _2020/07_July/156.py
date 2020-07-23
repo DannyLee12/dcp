cfe9e7803f31802766402520727c20bdfbce8ef2
Daniel Lee <daniel@dotmodus.com>
2020-07-23 09:37:52 +0100

    :sparkles: Find the fewest numbers to create an integer
    Algorithm: Start with the sqrt of n, convert to an integer.
    Note if sqrt is slow, and it might well ve, we can use a
    quicker algorithm to get the an integer relatively close
    with a binary search type.
    
    Decrement that value until it's less than the value looking
    for when squared, when this is the case do the same with the
    next value until the value is found. Note we will always find
    a solution by using 1s.

---
+++ _2020/07_July/156.py
@@ -0,0 +1,30 @@
+"""
+Given a positive integer n, find the smallest number of squared integers
+which sum to n.
+
+For example, given n = 13, return 2 since 13 = 32 + 22 = 9 + 4.
+
+Given n = 27, return 3 since 27 = 32 + 32 + 32 = 9 + 9 + 9.
+"""
+from math import sqrt
+
+
+def get_integers(n: int) -> int:
+    """Return the fewest number of squared integers that add up to n"""
+    l = [int(sqrt(n))]
+    val = l[0] * l[0]
+    index = 0
+    while val != n:
+        val = sum([x*x for x in l])
+        if val > n:
+            l[index] -= 1
+        elif val < n:
+            index += 1
+            l.append(l[index - 1])
+
+    return len(l)
+
+
+if __name__ == '__main__':
+    assert get_integers(13) == 2
+    assert get_integers(27) == 3
