54491fa7c9d479fa0b4eb61c6d748ad489826467
Daniel Lee <daniel@dotmodus.com>
2020-08-04 09:31:47 +0100

    :zap: Improve space complexity by removing copies of the list
    No need for a copy of the list, only extra space is swap space
    for the element to which we are concerned.

--- _2020/08_August/168.py
+++ _2020/08_August/168.py
@@ -15,26 +15,27 @@ you should return:
 
 Follow-up: What if you couldn't use any extra space?
 """
-from copy import deepcopy
 
 
 def rotate_90(l: list) -> list:
     """Rotate a N x N matrix by 90 degrees"""
     N = len(l)
-    new_list = [[0]* N for _ in range(N)]
 
     def transpose(l):
         for x in range(N):
-            for y in range(N):
-                new_list[x][y] = l[y][x]
-        return new_list
+            for y in range(x, N):
+                t = l[x][y]
+                l[x][y] = l[y][x]
+                l[y][x] = t
+        return l
 
     def reverse_columns(l):
-        new_list = deepcopy(l)
         for i in range(N):
-            for j in range(N):
-                new_list[i][j] = l[i][N - j - 1]
-        return new_list
+            for j in range(N//2):
+                t = l[i][j]
+                l[i][j] = l[i][N - j - 1]
+                l[i][N - j - 1] = t
+        return l
 
     return reverse_columns(transpose(l))
 
@@ -43,3 +44,11 @@ if __name__ == '__main__':
     assert rotate_90([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == [[7, 4, 1],
                                                             [8, 5, 2],
                                                             [9, 6, 3]]
+
+    assert rotate_90([[1, 2, 3, 4],
+                     [5, 6, 7, 8],
+                     [9, 10, 11, 12],
+                     [13, 14, 15, 16]]) == [[13, 9, 5, 1],
+                                            [14, 10, 6, 2],
+                                            [15, 11, 7, 3],
+                                            [16, 12, 8, 4]]
