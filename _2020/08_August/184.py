6e5d82055aa270c53ce65901483614f2b3763ea4
Daniel Lee <daniel@dotmodus.com>
2020-08-20 08:42:09 +0100

    :sparkles: Roll my own gcd algo
    - if a == b, return a
    - Switch a and b so that b is always a
    - If a is divisible by b
    - Else recursively add small amounts to both a and b
      until the conditions are met

--- _2020/08_August/184.py
+++ _2020/08_August/184.py
@@ -4,7 +4,22 @@ Given n numbers, find the greatest common denominator between them.
 For example, given the numbers [42, 56, 14], return 14.
 """
 
-from math import gcd
+
+def gcd(a, b, ap=None, bp=None):
+    """Return the gcd of a and b"""
+    if a == b:
+        return a
+    elif a < b:  # If a < b, switch a and b
+        a, b = b, a
+
+    if a % b == 0:
+        return b
+    if not ap and not bp:
+        ap, bp = a, b
+    if b + bp <= a:
+        return gcd(a, b + bp, ap, bp)
+    else:
+        return gcd(a + ap, b + bp, ap, bp)
 
 
 def greatest_common_denominator(l: list) -> int:
