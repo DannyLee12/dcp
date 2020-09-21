f7db7ac5b202e696aa98b1c56d611babff57c42d
Daniel Lee <daniel@dotmodus.com>
2020-09-21 09:01:10 +0100

    :art: Improve solution
    - Use bit shifting and bitwise operations to save the extra space
      used by the string.
    - Same time complexity, O(len(bin(n))).

--- _2020/09_September/214.py
+++ _2020/09_September/214.py
@@ -9,12 +9,13 @@ For example, given 156, you should return 3.
 def consecutive_ones(n: int) -> int:
     """Return the longest consecutive 1s in a binary representation"""
     max_length, length = 0, 0
-    for x in bin(n)[2:]:
-        if x == '1':
+    while n:
+        if n & 1:
             length += 1
         else:
             max_length = max(max_length, length)
             length = 0
+        n = n >> 1
 
     return max_length
 
