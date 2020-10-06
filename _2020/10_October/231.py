4c7d69fb50546bf3d2d88bf750829e0b12cd1a4c
Daniel Lee <daniel@dotmodus.com>
2020-10-06 09:32:49 +0100

    :zap: Improve performance
    - This may be quicker since in the worst-case since it checks
      a dictionary for the values.
    - In some cases, this will be much slower

--- _2020/10_October/231.py
+++ _2020/10_October/231.py
@@ -5,30 +5,29 @@ adjacent characters are the same. If this is not possible, return None.
 For example, given "aaabbc", you could return "ababac". Given "aaab", return
 None.
 """
+from collections import defaultdict
 
 
 def rearrange(s: str) -> str:
     """Rearrange a string so that no adjacent letters are the same"""
-    ns = s[0]
-    s = s[1:]
+    d = defaultdict(int)
+    for x in s[1:]:
+        d[x] += 1
 
-    while s:
-        ns_letter = ns[-1]
-        found = False
-        for i, l in enumerate(s):
-            if l != ns_letter:
-                ns += l
-                # Remove the letter from s
-                s = s[:i] + s[i+1:]
-                found = True
-                break
-        # If no other letter is found
-        if not found:
+    s2 = s[0]
+    while any(x > 0 for x in d.values()):
+        changed = False
+        for k in d.keys():
+            if k != s2[-1] and d[k] > 0:
+                s2 += k
+                d[k] -= 1
+                changed = True
+        if not changed:
             return None
 
-    return ns
+    return s2
 
 
 if __name__ == '__main__':
-    assert rearrange("aaabbc") == "ababac"
+    assert rearrange("aaabbc") == "abcaba"
     assert rearrange("aaab") is None
